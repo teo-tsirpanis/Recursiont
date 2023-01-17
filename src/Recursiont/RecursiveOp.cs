2460914b0f348facf978568782fd23882b219ba9
Theodore Tsirpanis <teo@tsirpanis.gr>
2023-01-18 00:58:55 +0200

    Stop relying on `global using Recursiont.Infrastructure;`.

--- src/Recursiont/RecursiveOp.cs
+++ src/Recursiont/RecursiveOp.cs
@@ -7,6 +7,7 @@ using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 using System.Runtime.ExceptionServices;
+using Recursiont.Infrastructure;
 
 namespace Recursiont;
 
